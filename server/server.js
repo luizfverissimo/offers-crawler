b9a8b0a464f7d5b9ed42884d49cc67bff1e0218f
Luiz Fernando Verissimo <luizfverissimo@gmail.com>
2021-09-11 19:05:52 -0300

    fixes no docker

--- server/server.js
+++ server/server.js
@@ -1,16 +1,30 @@
-import express from 'express'
+const express = require('express');
+const puppeteer = require('puppeteer');
 
-const app = express()
+require('dotenv').config()
 
-app.get('/', (req, res) => {
-  console.log('request received!')
-  const message= {
-    sender: 'LF Verissimo',
-    message: 'Olá Mundão!'
+const app = express();
+
+app.get('/', async (req, res) => {
+  console.log('request received!');
+
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    page.goto('https://www.ofertaesperta.com/#ofertas');
+  
+    console.log(await page.content());
+    const image = await page.screenshot({path: 'print.png'});
+  
+    await browser.close();
+  
+    res.send(image);
+  } catch(err) {
+    res.status = 505
+    res.send(err)
   }
-  res.json(message)
-})
+});
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server app listening at http://localhost:${process.env.PORT}`)
-})
+  console.log(`Server app listening at http://localhost:${process.env.PORT}`);
+});
