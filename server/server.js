af5f5a65a5b81cd4c1eaed9075b01ab39c48dc2a
Luiz Fernando Veríssimo <62751844+luizfverissimo@users.noreply.github.com>
2021-09-11 21:08:10 -0300

    get title logic DONE

--- server/server.js
+++ server/server.js
@@ -1,28 +1,68 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
 
-require('dotenv').config()
+require('dotenv').config();
 
 const app = express();
 
+async function getText(selector, page) {
+  const text = await page.$$eval(selector, elements => elements.map(item => item.textContent))
+  return text
+}
+
+// async function getUrl(selector, page) {
+//   await page.$$eval(selector, elements => elements.map(item => item.textContent))
+// }
+
+
 app.get('/', async (req, res) => {
   console.log('request received!');
-
-  try {
+  const offers = []
+  
+  // try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    page.goto('https://www.ofertaesperta.com/#ofertas');
-  
-    console.log(await page.content());
-    const image = await page.screenshot({path: 'print.png'});
-  
+    await page.goto(process.env.TARGET_URL);
+
+    const cards = await page.$$('.card-col')
+
+    const cardsPromises = cards.map(async (card, index) => {
+      return new Promise(async (res, rej) => {
+        const title = await card.$eval('.offer-title', el => el.textContent)
+        console.log(title)
+        res()
+      })
+    })
+
+    await Promise.all(cardsPromises)
+
+    // const titles = await getText('.offer-title', page)
+    // const offerPrices = await getText('.offer-card-price', page)
+    // const previousPrices = await getText('.offer-previous-price', page)
+
+    // titles.forEach((title, index) => {
+    //   const offer = {
+    //     title,
+    //     offerPrice: offerPrices[index],
+    //     previousPrice: previousPrices[index],
+    //   }
+
+    //   offers.push(offer)
+    // })
+
+    // console.log(offers)
+
+    // offers.forEach(offer => {
+    //   if(offer.title.includes('Match')) {
+
+    //   }
+    // })
+
     await browser.close();
-  
-    res.send(image);
-  } catch(err) {
-    res.status = 505
-    res.send(err)
-  }
+    res.send('Procedimento completo');
+  // } catch (err) {
+  //   res.status(500).send(err);
+  // }
 });
 
 app.listen(process.env.PORT, () => {
